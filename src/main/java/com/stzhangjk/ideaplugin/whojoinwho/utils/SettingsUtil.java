40299e392dedfdbd9bb139bdc4026c95bf5c7bbe
stzhangjk <stzhangjk@163.com>
2024-05-03 04:39:25 +0800

    fix: 处理判空

--- src/main/java/com/stzhangjk/ideaplugin/whojoinwho/utils/SettingsUtil.java
+++ src/main/java/com/stzhangjk/ideaplugin/whojoinwho/utils/SettingsUtil.java
@@ -3,16 +3,16 @@ package com.stzhangjk.ideaplugin.whojoinwho.utils;
 import com.intellij.execution.util.EnvironmentVariable;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SettingsUtil {
 
-    public static String stringifyProps(@NotNull Map<String, String> props) {
-        if (props.isEmpty()) {
+    public static String stringifyProps(Map<String, String> props) {
+        if (props == null || props.isEmpty()) {
             return "";
         }
         StringBuilder buf = new StringBuilder();
@@ -26,12 +26,18 @@ public class SettingsUtil {
     }
 
     public static String stringifyProps(List<EnvironmentVariable> envs) {
+        if (envs == null || envs.isEmpty()) {
+            return "";
+        }
         return envs.stream()
                 .map(prop -> String.format("%s=%s", StringUtil.escapeChar(prop.getName(), ';'), StringUtil.escapeChar(prop.getValue(), ';')))
                 .collect(Collectors.joining(";"));
     }
 
     public static List<EnvironmentVariable> convertToVariables(Map<String, String> map, final boolean readOnly) {
+        if (map == null || map.isEmpty()) {
+            return new ArrayList<>();
+        }
         return ContainerUtil.map(map.entrySet(), entry -> new EnvironmentVariable(entry.getKey(), entry.getValue(), readOnly) {
             @Override
             public boolean getNameIsWriteable() {
