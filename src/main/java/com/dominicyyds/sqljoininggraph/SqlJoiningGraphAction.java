eaea5dee204539873ccab9fe3a0e04666d58d1b6
dominicyyds <dominicyyds@163.com>
2024-08-04 11:50:49 +0800

    style: 删除多余import

--- src/main/java/com/dominicyyds/sqljoininggraph/SqlJoiningGraphAction.java
+++ src/main/java/com/dominicyyds/sqljoininggraph/SqlJoiningGraphAction.java
@@ -29,7 +29,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 public class SqlJoiningGraphAction extends AnAction {
 
