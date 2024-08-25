242fcadb8fdaba220f1022f34611b247017eb042
dominicyyds <dominicyyds@163.com>
2024-08-26 01:31:09 +0800

    feat: 添加PsiReferenceExpression.resolve()得到PsiLocalVariable支持

--- src/main/java/com/dominicyyds/sqljoininggraph/computers/PsiReferenceStringConstComputer.java
+++ src/main/java/com/dominicyyds/sqljoininggraph/computers/PsiReferenceStringConstComputer.java
@@ -2,6 +2,7 @@ package com.dominicyyds.sqljoininggraph.computers;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiLocalVariable;
 import com.intellij.psi.PsiReferenceExpression;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,9 +15,16 @@ public class PsiReferenceStringConstComputer implements PsiStringConstComputer<P
     @Override
     public @NotNull List<String> compute(PsiReferenceExpression psiElement) {
         PsiElement resolved = psiElement.resolve();
-        if (!(resolved instanceof PsiField)) {
-            return List.of();
+        if (resolved instanceof PsiField && ((PsiField) resolved).getInitializer() != null) {
+            return PsiVariableStringConstComputer.INSTANCE.compute((PsiField) resolved);
         }
-        return PsiVariableStringConstComputer.INSTANCE.compute((PsiField) resolved);
+        if (resolved instanceof PsiLocalVariable && ((PsiLocalVariable) resolved).hasInitializer()) {
+            return PsiVariableStringConstComputer.INSTANCE.compute((PsiLocalVariable) resolved);
+        }
+        //TODO 支持两次赋值或跳转
+        //String midd;
+        //midd = "union all\n";
+        //String sql = "12312" + midd + "123123";
+        return List.of();
     }
 }
